e756c6207732f82c502900b1ee1b58609ba4ea81
cirdans-home <f.durastante@na.iac.cnr.it>
2019-11-07 09:08:25 +0100

    Routine for comparing shortest path and logarithimc distances

---
+++ comparedistances.m
@@ -0,0 +1,40 @@
+%% COMPARE DISTANCES FOR A DIGRAPH \Gamma
+
+clear; clc; close all;
+
+load('TestGraphs/gre_115.mat');
+A = spones(Problem.A);
+n = size(A,1);
+
+% Building the digraph:
+G = digraph(A,'omitselfloops');
+
+% Shortest-path distance
+W1 = G.distances();
+
+% Logarithmic distance
+e = ones(n,1);
+I = speye(n,n);
+L = spdiags(A*e,0,n,n) - A;
+S = inv(I+L);
+[row,col] = find(S < 0); %Check if there are numerical negative entries
+S(row,col) = 0;
+H = log(S);
+h = diag(H);
+U = h*e' - H;
+
+W2 = 0.5*(U+U.');
+
+%% PLOT ROUTINES
+
+figure(1);
+[ha, pos] = tight_subplot(1, 3,[.01 .03],[.1 .01],[.01 .01]);
+axes(ha(1)); spy(A,'s'); xticks([]); yticks([]); title('Adjacency'); set(gca,'FontSize',14)
+axes(ha(2)); spyc(W1,'autumn',1); xticks([]); yticks([]); title('Shortest Path'); set(gca,'FontSize',14)
+axes(ha(3)); spyc(W2,'autumn',1); xticks([]); yticks([]); title('Logarithmic'); set(gca,'FontSize',14)
+set(gcf,'color','white');
+
+figure(2);
+[hb, posb] = tight_subplot(1, 2,[.01 .03],[.1 .01],[.01 .01]);
+axes(hb(1)); spy(L);
+axes(hb(2)); spyc(S,'autumn',1); xticks([]); yticks([]); title('Logarithmic'); set(gca,'FontSize',14)
