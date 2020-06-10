0392c5dadb4f08057801df260042aa9048a5d854
ftudisco <tudisco.francesco@gmail.com>
2020-06-10 18:20:02 +0100

    csv results

--- Kfold_crossValidation.m
+++ Kfold_crossValidation.m
@@ -38,8 +38,15 @@ clear; clc;
 TestGraph={'adjnoun', 'USAir97', 'EuairComplete','football',...
            'celegans_metabolic','polbooks','mycielskian9',...
            'delaunay_n10','data','USpowerGrid'};
-%TestGraph={'adjnoun'};
+% TestGraph={'adjnoun'};
 Result=zeros(length(TestGraph),2);
+
+Results_TAB = [];
+
+
+rng(100)
+
+
 for problem=1:length(TestGraph)
 
 load(['TestGraphs/',TestGraph{problem},'.mat']);
@@ -67,12 +74,19 @@ c_array = [0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.85 0.9 0.99 ];  % pagerank teleportatio
 %alpha_array = [.4 .5 Inf];   
 %c_array     = [.1 .85 .9]; 
 tau         =  0.1;  % percentage of removed edges
-TestGraph{problem}
+fprintf('Dataset = %s \t |V| = %d \t |E| = %d \t |E|/|V| = %1.5f\n\n',TestGraph{problem},n,m,m/(n^2));
 NumTrial1=10;
 LocalScores = zeros(length(TestGraph), NumTrial1);
 NLocalScores= zeros(length(TestGraph),NumTrial1);
+
+num_nodes = n;
+num_edges = m;
+density = m/(n^2);
+dataset = string(TestGraph{problem});
+
+
 for nmt1=1:NumTrial1
-    ['Trial n: ',num2str(nmt1)]
+%     fprintf('Trial n: %d\n\n',nmt1);
     ind_deleted_edges_ToTrain=randperm(m);
     ind_deleted_edges_ToTrain=ind_deleted_edges_ToTrain(1:floor(tau*m));
     KT=length(ind_deleted_edges_ToTrain);
@@ -87,7 +101,7 @@ for nmt1=1:NumTrial1
     best_score=zeros(cV.NumTestSets,1);
     best_c_L=zeros(cV.NumTestSets,1);
     best_score_L=zeros(cV.NumTestSets,1);
-    for  nmt2=1:cV.NumTestSets
+    for nmt2=1:cV.NumTestSets
 
         ind_deleted_edges = find(cV.test(nmt2)==1);                          
         K = length(ind_deleted_edges);
@@ -111,20 +125,35 @@ for nmt1=1:NumTrial1
         [aL,bL]=max(score(:,end));
         best_c_L(nmt2)=c_array(bL(1));
         best_score_L(nmt2)=aL(1);
+        
     end
+    
     % NonLocal
     [Snl,INTSnl]=max(best_score);
-    cnl=best_c(INTSnl);
-    alpha1=best_alpha(INTSnl);
+    cnl=best_c(INTSnl); % best value of c for this trial
+    alpha1=best_alpha(INTSnl); % best value of alpha for this trial
     % Local
     [Sl,INTSl]=max(best_score_L);
-    cl=best_c_L(INTSl);
+    cl=best_c_L(INTSl); % best value of c for this trial
     [~,NLocalScores(problem,nmt1)] = Predict(G,ind_deleted_edges_ToTrain,...
                              cnl(1),alpha1,KT,method,symmetry);
     NLocalScores(problem,nmt1)=NLocalScores(nmt1)/KT;
     [~,LocalScores(problem,nmt1)] = Predict(G,ind_deleted_edges_ToTrain,...
                             cl(1),Inf,KT,method,symmetry);
     LocalScores(problem,nmt1)=LocalScores(nmt1)/KT;
+    
+%     fprintf('alpha  =  %1.3f \t cnl = %1.3f \t cl =%1.3f \n', alpha1, cnl, cl)
+%     fprintf('Local = %1.5f \t Nonlocal = %1.5f\n\n', LocalScores(problem,nmt1),  NLocalScores(problem,nmt1));
+
+    trial = nmt1;
+    accuracy_local = LocalScores(problem,nmt1);
+    accuracy_nonlocal = NLocalScores(problem,nmt1);
+    alpha = alpha1;
+    c_nonlocal = cnl;
+    c_local = cl;
+    
+    Results_TAB = [Results_TAB; table(dataset, num_nodes, num_edges, density, trial, alpha, c_nonlocal, accuracy_nonlocal, c_local, accuracy_local)];
+    disp(Results_TAB(:,5:end))
 end
 LM=sum(LocalScores(problem,:),2)/NumTrial1;
 NLM=sum(NLocalScores(problem,:),2)/NumTrial1;
@@ -135,7 +164,8 @@ NLM=sum(NLocalScores(problem,:),2)/NumTrial1;
 %h.YLabel = 'Teleportation';
 
 Result(problem,1)=LM;
-Result(problem,2)=NLM
+Result(problem,2)=NLM;
 
 end
-save KFoldCVM Result LocalScores NLocalScores
+save KFoldCVM Results_TAB Result LocalScores NLocalScores
+writetable(Results_TAB,'results.csv')
